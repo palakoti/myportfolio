c57a0f20f0f833fdddf4a210b25c72ade021848e
Swetha Palakoti <swethapalakoti@outlook.com>
2020-05-11 13:29:29 -0500

    Project Structure

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import AppHeader from './AppHeader.js';
+import AppHeader from './components/AppHeader';
 import './App.css';
-import Home from './Home.js';
-import Introduction from './Introduction.js';
-import Skills from './Skills.js';
+import Home from './components/Home';
+import Introduction from './components/Introduction';
+import Skills from './components/Skills';
 import Card from '@material-ui/core/Card';
 
 class App extends Component {
